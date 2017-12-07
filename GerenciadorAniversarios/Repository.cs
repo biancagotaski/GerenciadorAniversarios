aed9281bcfbd93c73005c0cec87d66d946584ba0
Bianca Gotaski <biancagotaski@outlook.com>
2017-12-07 18:41:58 -0200

    incrementando metodo de listar por nome. Métodos ainda incompletos.

---
+++ GerenciadorAniversarios/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorAniversarios
+{
+    class Repository
+    {
+        private List<Person> Persons = new List<Person>();
+
+        private void addPerson(Person person)
+        {
+            Persons.Add(person);
+        }
+
+        public int consultBirthday(string name)
+        {
+            foreach(var choosePerson in Persons)
+            {
+                if(name === choosePerson)
+                {
+
+                }
+            }
+            return 
+        }
+
+        public string consultName(string name)
+        {
+            foreach (string p in Persons)
+            {
+                if (p == name)
+                {
+                    return name;
+                }
+            }
+        }
+    }
+}
